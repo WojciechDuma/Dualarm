93be1e091557014c829d08c89b935cce702ca910
Wojciech Duma <wojciech.michal.duma@gmail.com>
2020-02-25 12:53:10 +0100

    Fixed error and typo, and correct shop url

--- js/certificates.js
+++ js/certificates.js
@@ -1,25 +1,24 @@
 // Certyficate:
 
 const showCertificates = () => {
-    const allImg = [...document.querySelectorAll('.certificates img')];
-    const show = document.querySelector('.show');
-    const showImg = document.querySelector('.show img');
-    // const close = document.querySelector('.close');
-    allImg.forEach(img => {
-        img.addEventListener('click', () => {
-            show.classList.add('active');
-            const imgSrc = img.getAttribute('src');
-            showImg.setAttribute('src', imgSrc);
-        });
-    });
+  const allImg = [...document.querySelectorAll(".certificates img")];
+  const show = document.querySelector(".show");
+  const showImg = document.createElement("img");
+  show.appendChild(showImg);
 
-    // close.addEventListener('click', () => {
-    //   show.classList.remove('active');
-    // });
-    show.addEventListener('click', () => {
-        show.classList.remove('active');
+  allImg.forEach(img => {
+    img.addEventListener("click", () => {
+      show.classList.add("active");
+      const imgSrc = img.getAttribute("src");
+      const imgAlt = img.getAttribute("alt");
+      showImg.setAttribute("src", imgSrc);
+      showImg.setAttribute("alt", imgAlt);
     });
+  });
+
+  show.addEventListener("click", () => {
+    show.classList.remove("active");
+  });
 };
-
 
 showCertificates();
